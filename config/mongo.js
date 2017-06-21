c365d61f091999fdd41eeb373e9b697ba168ee27
root <root@hydra-old.ibstower.com>
2017-06-21 06:47:31 -0400

    first launch

--- config/mongo.js
+++ config/mongo.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var mongoURI = "mongodb://localhost:27017/cms";
+var mongoURI = "mongodb://172.21.1.146:27017/cms";
 
 module.exports = {
     init: function () {
